00036c6cd71d8f19b1f7f1092006dcc0d4d3cdc2
Reza Amiri <reza.amiri.ac@gmail.com>
2025-02-28 10:10:21 +0300

    write writekey service for middleware EnsureWritekeyIsValid

---
+++ source/service/writekey/service_test.go
@@ -0,0 +1,45 @@
+package writekey_test
+
+import (
+	"context"
+	"fmt"
+	"testing"
+
+	"github.com/rezaAmiri123/ormus/source/service/writekey"
+)
+
+type mockRepo struct {
+}
+
+// TODO - use https://github.com/golang/mock
+func (m mockRepo) IsValidWriteKey(ctx context.Context, writeKey string) (bool, error) {
+	if writeKey == "" {
+		return false, fmt.Errorf("writekey not found")
+	}
+	return true, nil
+}
+
+func TestIsValid(t *testing.T) {
+	t.Run("writekey not found", func(t *testing.T) {
+		m := new(mockRepo)
+		service := writekey.New(m)
+		ctx := context.Background()
+		_, err := service.IsValid(ctx, "")
+		if err == nil {
+			t.Fatal("error is nil")
+		}
+	})
+
+	t.Run("writekey is exists and valid", func(t *testing.T) {
+		m := new(mockRepo)
+		service := writekey.New(m)
+		ctx := context.Background()
+		isValid, err := service.IsValid(ctx, "asdfffg4g5g56d5s4s6s5sd8")
+		if err != nil {
+			t.Fatal("error is not nil")
+		}
+		if !isValid {
+			t.Fatal("writekey is not valid")
+		}
+	})
+}
