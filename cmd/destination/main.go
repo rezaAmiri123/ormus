0875b3bf95150d6f77ed1c492f69c4617daa7a68
Reza Amiri <reza.amiri.ac@gmail.com>
2025-03-03 18:17:40 +0300

    feat(destination): Add new task manager adapter with channel driver

--- cmd/destination/main.go
+++ cmd/destination/main.go
@@ -1,6 +1,9 @@
 package main
 
 import (
+	"github.com/rezaAmiri123/ormus/destination/dconfig"
+	"github.com/rezaAmiri123/ormus/pkg/channel"
+	rbbitmqchannel "github.com/rezaAmiri123/ormus/pkg/channel/adapter/rabbitmq"
 	"log"
 	"log/slog"
 	"os"
@@ -11,7 +14,7 @@ import (
 	"github.com/rezaAmiri123/ormus/config"
 	"github.com/rezaAmiri123/ormus/destination/processedevent/adapter/rabbitmqconsumer"
 	"github.com/rezaAmiri123/ormus/destination/taskcoordinator/adapter/dtcoordinator"
-	"github.com/rezaAmiri123/ormus/destination/taskmanager/adapter/rabbitmqtaskmanager"
+	"github.com/rezaAmiri123/ormus/destination/taskmanager/adapter/rabbitmqchanneltaskmanager"
 	"github.com/rezaAmiri123/ormus/logger"
 	"github.com/rezaAmiri123/ormus/manager/entity"
 )
@@ -70,8 +73,32 @@ func main() {
 	// destination type coordinator which means every task with specific destination type
 	// will be published to its corresponding task publisher.
 
+	reconnectSecond := 5
+	channelSize := 100
+	numberInstant := 5
+	maxRetryPolicy := 5
+
 	taskPublisherCnf := config.C().Destination.RabbitMQTaskManagerConnection
-	webhookTaskPublisher := rabbitmqtaskmanager.NewTaskPublisher(taskPublisherCnf, "webhook_tasks_queue")
+
+	inputChannelAdapter := rbbitmqchannel.New(done, &wg, dconfig.RabbitMQConsumerConnection{
+		User:            taskPublisherCnf.User,
+		Password:        taskPublisherCnf.Password,
+		Host:            taskPublisherCnf.Host,
+		Port:            taskPublisherCnf.Port,
+		Vhost:           "/",
+		ReconnectSecond: reconnectSecond,
+	})
+
+	webHookQueueName := "webhook_tasks"
+
+	inputChannelAdapter.NewChannel(webHookQueueName, channel.InputOnlyMode, channelSize, numberInstant, maxRetryPolicy)
+
+	inputChannel, err := inputChannelAdapter.GetInputChannel(webHookQueueName)
+	if err != nil {
+		log.Fatalf("Couldn't get input channel for %s: %s", webHookQueueName, err)
+	}
+
+	webhookTaskPublisher := rabbitmqchanneltaskmanager.NewTaskPublisher(inputChannel)
 
 	taskPublishers := make(dtcoordinator.TaskPublisherMap)
 	taskPublishers[entity.WebhookDestinationType] = webhookTaskPublisher
