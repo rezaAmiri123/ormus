d90a2e9edb1c05f94e6f968d1921451381b4c6cb
Reza Amiri <reza.amiri.ac@gmail.com>
2025-03-13 16:32:03 +0300

    feat: protobuf

--- adapter/scylladb/scyllainitialize/db.go
+++ adapter/scylladb/scyllainitialize/db.go
@@ -10,8 +10,8 @@ import (
 	"time"
 
 	"github.com/gocql/gocql"
-	"github.com/ormushq/ormus/adapter/scylladb"
-	"github.com/ormushq/ormus/logger"
+	"github.com/rezaAmiri123/ormus/adapter/scylladb"
+	"github.com/rezaAmiri123/ormus/logger"
 )
 
 type ScyllaDBConnection struct {
