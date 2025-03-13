c1d2ecfdd65580497d567fd3e2d44d3f72fcd665
Reza Amiri <reza.amiri.ac@gmail.com>
2025-03-13 17:31:37 +0300

    fix(ormus): add new helper to otela package

--- source/delivery/httpserver/server.go
+++ source/delivery/httpserver/server.go
@@ -4,6 +4,8 @@ import (
 	"fmt"
 
 	"github.com/labstack/echo/v4"
+	"github.com/labstack/echo/v4/middleware"
+	"github.com/rezaAmiri123/ormus/adapter/otela"
 	"github.com/rezaAmiri123/ormus/source"
 )
 
@@ -35,6 +37,20 @@ func (s Server) Serve() {
 	for _, h := range s.handlers {
 		h.SetRoutes(s.Router)
 	}
+	s.Router.Use(middleware.RequestLoggerWithConfig(middleware.RequestLoggerConfig{
+		LogURI:           true,
+		LogStatus:        true,
+		LogHost:          true,
+		LogRemoteIP:      true,
+		LogRequestID:     true,
+		LogMethod:        true,
+		LogContentLength: true,
+		LogResponseSize:  true,
+		LogLatency:       true,
+		LogError:         true,
+		LogProtocol:      true,
+		LogValuesFunc:    otela.EchoRequestLoggerLogValuesFunc("httpserver-source", "Serve"),
+	}))
 
 	port := fmt.Sprintf(":%d", s.config.HTTPServer.Port)
 	s.Router.Logger.Fatal(s.Router.Start(port))
