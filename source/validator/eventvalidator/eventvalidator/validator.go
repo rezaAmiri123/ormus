f306da187230ec1989a32a8f8a6083171c3b2b5e
Reza Amiri <reza.amiri.ac@gmail.com>
2025-03-14 19:09:32 +0300

    feat(source): implement unit tests for source

--- source/validator/eventvalidator/eventvalidator/validator.go
+++ source/validator/eventvalidator/eventvalidator/validator.go
@@ -3,13 +3,11 @@ package eventvalidator
 import (
 	"context"
 
-	proto_source "github.com/rezaAmiri123/ormus/contract/go/source"
 	"github.com/rezaAmiri123/ormus/source"
 )
 
 type Repository interface {
 	IsWriteKeyValid(ctx context.Context, writeKey string, expirationTime uint) (bool, error)
-	CreateNewWriteKey(ctx context.Context, writeKey *proto_source.NewSourceEvent, expirationTime uint) error
 }
 
 type Validator struct {
